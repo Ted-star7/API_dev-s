1127859f43c96212cc8a95c274995df1aa6826ed
Teddy Kibuthu <kibuthuteddy@gmail.com>
2026-03-10 11:27:29 +0300

    stash

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -15,7 +15,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        return Customer::all();
+        return new CustomerCollection(Customer::all());
     }
 
     /**
@@ -39,7 +39,7 @@ class CustomerController extends Controller
      */
     public function show(Customer $customer)
     {
-        return $customer;
+        return new CustomerResource($customer);
     }
 
     /**
